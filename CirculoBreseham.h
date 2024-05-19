bb82336d3f612b92cd17a4652a9b304bc5ef6c6e
vick-genis <99092155+vick-genis@users.noreply.github.com>
2024-05-18 20:19:06 -0600

    Add files via upload

---
+++ CirculoBreseham.h
@@ -0,0 +1,23 @@
+#pragma once
+#ifndef CIRCULO_BRESEHAM_H
+#define CIRCULO_BRESEHAM_H
+#include <vector>
+#include <cmath>
+#include "definition.h" // Suponiendo que tienes definido el tipo VertexColor aquí
+
+class CirculoBreseham {
+public:
+    CirculoBreseham(float radius, int numSegments);
+    void init();
+    void load();
+    void render();
+    virtual ~CirculoBreseham();
+private:
+    std::vector<VertexColor> vertices;
+    std::vector<GLuint> indices;
+    float radius;
+    int numSegments;
+
+    GLuint VAO, VBO, EBO;
+};
+#endif // CIRCULO_BRESEHAM_H
